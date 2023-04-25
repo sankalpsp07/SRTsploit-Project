ede03fbe4d0c6249f93f699b334ab9ed30427070
Sankalp Sandeep Paranjpe <sankalp.paranjpe@gmail.com>
2023-04-26 00:23:05 +0530

    Update install_docker_bench.sh

--- install_docker_bench.sh
+++ install_docker_bench.sh
@@ -1,1 +1,2 @@
-
+print("Installing Docker Bench Security...")
+subprocess.run(["curl", "-sSL", "https://raw.githubusercontent.com/docker/docker-bench-security/master/docker-bench-security.sh", "-o", "/usr/local/bin/docker-bench-security"])
